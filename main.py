752398d059cad178d7543aa64ab65b1ca8c3b379
Zoraiz Ali Rafeh <135309450+MalikZoraiz@users.noreply.github.com>
2023-12-19 04:18:52 -0800

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 try:
     Age = int(input("Age: "))
+    income = 20000
+    risk = income/Age
     print(Age)
-
+except ZeroDivisionError:
+    print("Age can not be Zero")
 except ValueError:
-    print("Invalid Value")    
+    print("Invalid Value")
